5e9cff09ab316e5342bfecf3c59ade66bbad9365
Daniel Cross <danielcross2872@gmail.com>
2024-04-30 10:48:07 -0600

    Prep 5 Completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        void DisplayWelcome(){
+            Console.WriteLine("Welcome to the Program!");
+        }
+        string PromptUserName(){
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        int PromptUserNumber(){
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        int SquareNumber(int num){
+            int numSquared = num * num;
+            return numSquared;
+        }
+        void DisplayResult(string name, int numSquared){
+            Console.WriteLine($"{name}, the square of your number is {numSquared}");
+        }
+        void Main(){
+            DisplayWelcome();
+            string name = PromptUserName();
+            int num = PromptUserNumber();
+            DisplayResult(name, SquareNumber(num));
+        }
+        Main();
     }
 }
