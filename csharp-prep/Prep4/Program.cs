220b2eeb628bc56c05df0ff9ac6dfbace7216e69
Daniel Cross <danielcross2872@gmail.com>
2024-04-30 10:21:37 -0600

    Calculations Fix

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Declare variables
+        int num = -1;
+        List<int> nums = [];
+
+        // Opening line
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+
+        // Input numbers loop
+        while (num != 0){
+        Console.Write("Enter number: ");
+        num = int.Parse(Console.ReadLine());
+        if (num != 0){
+        nums.Add(num);
+        };
+        }
+
+        // Calculations
+        int total = 0;
+        int largest = 0;
+        foreach (int number in nums){
+            total += number;
+            if (number > largest){
+                largest = number;
+            }
+        }
+        float avg = total / nums.Count;
+
+        // Display answers
+        Console.WriteLine($"Total: {total}\nAverage: {Math.Round(avg,2)}\nLargest: {largest}");
+
     }
 }
